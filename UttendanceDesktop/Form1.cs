182d2259b2a420e58a3bce3f468b528219dee98a
Leah Joshua <leahjoshua1313@gmail.com>
2025-03-05 14:25:17 -0600

    testing winforms
    trying out basic winform functionalities and checking to see if it works properly after pulling from git on someone else's machine

---
+++ UttendanceDesktop/Form1.cs
@@ -0,0 +1,16 @@
+namespace UttendanceDesktop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String text = textBox1.Text;
+            MessageBox.Show("You entered: " + text);
+        }
+    }
+}
