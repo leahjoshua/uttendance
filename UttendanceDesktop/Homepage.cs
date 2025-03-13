e0a8f110bbe38f73fa46a4dc43adad880259ec99
rixarixaaa <111485215+rixarixaaa@users.noreply.github.com>
2025-03-13 17:03:51 -0500

    Homepage and AddCourse

---
+++ UttendanceDesktop/Homepage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+//parisa made this
+namespace UttendanceDesktop
+{
+    public partial class Homepage : Form
+    {
+        public Homepage()
+        {
+            InitializeComponent();
+        }
+
+        private void addCourseBtn_Click(object sender, EventArgs e)
+        {
+            AddCourse newCourse = new AddCourse();
+            newCourse.show();
+        }
+
+        private void dltCourseBtn_Click(object sender, EventArgs e)
+        {
+            DeleteCourse dltCourse = new DeleteCourse();
+            dltCourse.show();
+        }
+
+        private void CourseCard_Click(object sender, EventArgs e)
+        {
+            CourseCard
+        }
+
+    }
+}
