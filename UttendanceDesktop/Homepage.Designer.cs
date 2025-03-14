c58c104f7be115de28de31544b6feac682f87761
rixarixaaa <111485215+rixarixaaa@users.noreply.github.com>
2025-03-14 06:45:12 -0500

    UI of Homepage
    Managed to create Homepage UI.

---
+++ UttendanceDesktop/Homepage.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace UttendanceDesktop
+{
+    partial class Homepage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            addButton = new Button();
+            YourCoursesLabel = new Label();
+            SuspendLayout();
+            // 
+            // addButton
+            // 
+            addButton.BackColor = Color.FromArgb(234, 117, 7);
+            addButton.Location = new Point(1242, 683);
+            addButton.Name = "addButton";
+            addButton.Size = new Size(112, 58);
+            addButton.TabIndex = 0;
+            addButton.Text = "Click Me";
+            addButton.UseVisualStyleBackColor = false;
+            // 
+            // YourCoursesLabel
+            // 
+            YourCoursesLabel.AutoSize = true;
+            YourCoursesLabel.Font = new Font("Afacad Medium", 48F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            YourCoursesLabel.Location = new Point(3, 98);
+            YourCoursesLabel.Name = "YourCoursesLabel";
+            YourCoursesLabel.Size = new Size(541, 128);
+            YourCoursesLabel.TabIndex = 1;
+            YourCoursesLabel.Text = "Your Courses";
+            // 
+            // Homepage
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(166, 176, 230);
+            ClientSize = new Size(1379, 753);
+            Controls.Add(YourCoursesLabel);
+            Controls.Add(addButton);
+            Name = "Homepage";
+            Text = "Uttendance";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button addButton;
+        private Label label1;
+        private Label YourCoursesLabel;
+    }
+}
--- UttendanceDesktop/Homepage.cs
+++ UttendanceDesktop/Homepage.cs
@@ -1,76 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using MySql.Data.MySqlClient;
+using System.Windows.Forms;
 
-//parisa made this
 namespace UttendanceDesktop
 {
     public partial class Homepage : Form
     {
-        private bool optionsVisible = false;
-
         public Homepage()
         {
             InitializeComponent();
-            ConfigureInitialState();
-        }
-
-        private void ConfigureInitialState()
-        {
-            //Trying to set initial buttons (hidden)
-            addCourseBtn.Visible = false;
-            dltCourseBtn.Visible = false;
-
-            //Styling the button
-            plusBtn.FlatStyle = FlatStyle.Flat;
-            plusBtn.Font = new Font("Segoe UI", 14f, FontStyle.Bold);
-            plusBtn.Size = new Size(40, 40);
-        }
-
-        private void plusBtn_Click(object sender, EventArgs e)
-        {
-            //Toggle visibility
-            optionsVisible = !obtionsVisible;
-            addCourseBtn.Visible = optionsVisible;
-            dltCourseBtn.Visible = optionsVisible;
-        }
-
-        private void addCourseBtn_Click(object sender, EventArgs e)
-        {
-            using(AddCourse newCourse = new AddCourse())
-            {
-                if(newCourse.ShowDialog() == DialogResult.OK)
-                {
-                    //Refresh the course list after adding
-                    RefreshCourseCards();
-                }
-            }
-        }
-
-        private void dltCourseBtn_Click(object sender, EventArgs e)
-        {
-            using (DeleteCourse dltCourse = new DeleteCourse())
-            {
-                if (dltCourse.ShowDialog() == DialogResult.OK)
-                {
-                    //Refresh the course list after adding
-                    RefreshCourseCards();
-                }
-            }
-        }
-
-        private void RefreshCourseCards()
-        {
-
-        }
-
-        private void CourseCard_Click(object sender, EventArgs e)
-        {
-            var clickCard = (Panel)sender;
         }
 
     }
