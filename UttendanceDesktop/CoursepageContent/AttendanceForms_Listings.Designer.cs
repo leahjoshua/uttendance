cc997e5b9a24faa8b2d45cee170148e112337804
AendriSingh <aendri4@gmail.com>
2025-04-03 21:13:47 -0500

    Quiz listing page
    display quizzes, filter

--- UttendanceDesktop/CoursepageContent/AttendanceForms_Listings.Designer.cs
+++ UttendanceDesktop/CoursepageContent/AttendanceForms_Listings.Designer.cs
@@ -31,6 +31,15 @@ namespace UttendanceDesktop
         private void InitializeComponent()
         {
             attendanceFormPagePanel = new Panel();
+            button1 = new Button();
+            filterButton = new Button();
+            attendanceflowLayoutPanel = new FlowLayoutPanel();
+            dateTimePicker = new DateTimePicker();
+            dateLabel = new Label();
+            filterLabel = new Label();
+            filterDropdown = new ComboBox();
+            statusDropDown = new ComboBox();
+            statusLabel = new Label();
             listingsLabel = new Label();
             attendanceFormsLabel = new Label();
             attendanceFormPagePanel.SuspendLayout();
@@ -38,22 +47,130 @@ namespace UttendanceDesktop
             // 
             // attendanceFormPagePanel
             // 
+            attendanceFormPagePanel.Controls.Add(button1);
+            attendanceFormPagePanel.Controls.Add(filterButton);
+            attendanceFormPagePanel.Controls.Add(attendanceflowLayoutPanel);
+            attendanceFormPagePanel.Controls.Add(dateTimePicker);
+            attendanceFormPagePanel.Controls.Add(dateLabel);
+            attendanceFormPagePanel.Controls.Add(filterLabel);
+            attendanceFormPagePanel.Controls.Add(filterDropdown);
+            attendanceFormPagePanel.Controls.Add(statusDropDown);
+            attendanceFormPagePanel.Controls.Add(statusLabel);
             attendanceFormPagePanel.Controls.Add(listingsLabel);
             attendanceFormPagePanel.Controls.Add(attendanceFormsLabel);
             attendanceFormPagePanel.Dock = DockStyle.Fill;
             attendanceFormPagePanel.Location = new Point(0, 0);
+            attendanceFormPagePanel.Margin = new Padding(3, 4, 3, 4);
             attendanceFormPagePanel.Name = "attendanceFormPagePanel";
-            attendanceFormPagePanel.Size = new Size(800, 450);
+            attendanceFormPagePanel.Size = new Size(914, 600);
             attendanceFormPagePanel.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            button1.BackColor = Color.FromArgb(0, 192, 0);
+            button1.FlatAppearance.BorderColor = Color.White;
+            button1.FlatAppearance.BorderSize = 2;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Location = new Point(862, 548);
+            button1.Name = "button1";
+            button1.Size = new Size(40, 40);
+            button1.TabIndex = 13;
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // filterButton
+            // 
+            filterButton.BackColor = Color.FromArgb(255, 128, 0);
+            filterButton.FlatAppearance.BorderColor = Color.FromArgb(192, 64, 0);
+            filterButton.FlatAppearance.BorderSize = 2;
+            filterButton.FlatAppearance.MouseDownBackColor = Color.FromArgb(192, 64, 0);
+            filterButton.FlatStyle = FlatStyle.Flat;
+            filterButton.Location = new Point(799, 170);
+            filterButton.Name = "filterButton";
+            filterButton.Size = new Size(27, 27);
+            filterButton.TabIndex = 12;
+            filterButton.UseVisualStyleBackColor = false;
+            filterButton.Click += filterButton_Click;
+            // 
+            // attendanceflowLayoutPanel
+            // 
+            attendanceflowLayoutPanel.Anchor = AnchorStyles.Top;
+            attendanceflowLayoutPanel.AutoScroll = true;
+            attendanceflowLayoutPanel.Location = new Point(72, 216);
+            attendanceflowLayoutPanel.Name = "attendanceflowLayoutPanel";
+            attendanceflowLayoutPanel.Size = new Size(779, 466);
+            attendanceflowLayoutPanel.TabIndex = 11;
+            // 
+            // dateTimePicker
+            // 
+            dateTimePicker.CalendarForeColor = Color.FromArgb(37, 42, 69);
+            dateTimePicker.CalendarTitleForeColor = Color.FromArgb(37, 42, 69);
+            dateTimePicker.Location = new Point(543, 170);
+            dateTimePicker.Name = "dateTimePicker";
+            dateTimePicker.Size = new Size(250, 27);
+            dateTimePicker.TabIndex = 10;
+            // 
+            // dateLabel
+            // 
+            dateLabel.AutoSize = true;
+            dateLabel.Font = new Font("Segoe UI", 10.25F);
+            dateLabel.ForeColor = Color.FromArgb(37, 42, 69);
+            dateLabel.Location = new Point(543, 141);
+            dateLabel.Name = "dateLabel";
+            dateLabel.Size = new Size(53, 25);
+            dateLabel.TabIndex = 8;
+            dateLabel.Text = "Date:";
+            // 
+            // filterLabel
+            // 
+            filterLabel.AutoSize = true;
+            filterLabel.Font = new Font("Segoe UI", 10.25F);
+            filterLabel.ForeColor = Color.FromArgb(37, 42, 69);
+            filterLabel.Location = new Point(386, 141);
+            filterLabel.Name = "filterLabel";
+            filterLabel.Size = new Size(54, 25);
+            filterLabel.TabIndex = 7;
+            filterLabel.Text = "Filter:";
+            // 
+            // filterDropdown
+            // 
+            filterDropdown.ForeColor = Color.FromArgb(37, 42, 69);
+            filterDropdown.FormattingEnabled = true;
+            filterDropdown.Items.AddRange(new object[] { "Before", "After", "On", "All" });
+            filterDropdown.Location = new Point(386, 169);
+            filterDropdown.Name = "filterDropdown";
+            filterDropdown.Size = new Size(151, 28);
+            filterDropdown.TabIndex = 5;
+            // 
+            // statusDropDown
+            // 
+            statusDropDown.ForeColor = Color.FromArgb(37, 42, 69);
+            statusDropDown.FormattingEnabled = true;
+            statusDropDown.Items.AddRange(new object[] { "Upcoming", "Open", "Closed", "All" });
+            statusDropDown.Location = new Point(72, 169);
+            statusDropDown.Name = "statusDropDown";
+            statusDropDown.Size = new Size(151, 28);
+            statusDropDown.TabIndex = 4;
+            // 
+            // statusLabel
+            // 
+            statusLabel.AutoSize = true;
+            statusLabel.Font = new Font("Segoe UI", 10.25F);
+            statusLabel.ForeColor = Color.FromArgb(37, 42, 69);
+            statusLabel.Location = new Point(72, 141);
+            statusLabel.Name = "statusLabel";
+            statusLabel.Size = new Size(64, 25);
+            statusLabel.TabIndex = 3;
+            statusLabel.Text = "Status:";
             // 
             // listingsLabel
             // 
             listingsLabel.AutoSize = true;
             listingsLabel.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
             listingsLabel.ForeColor = Color.FromArgb(37, 42, 69);
-            listingsLabel.Location = new Point(58, 69);
+            listingsLabel.Location = new Point(72, 100);
             listingsLabel.Name = "listingsLabel";
-            listingsLabel.Size = new Size(75, 25);
+            listingsLabel.Size = new Size(93, 32);
             listingsLabel.TabIndex = 1;
             listingsLabel.Text = "Listings";
             // 
@@ -61,21 +178,22 @@ namespace UttendanceDesktop
             // 
             attendanceFormsLabel.AutoSize = true;
             attendanceFormsLabel.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            attendanceFormsLabel.ForeColor = GlobalStyle.DARKEST_PURPLE;
-            attendanceFormsLabel.Location = new Point(43, 37);
+            attendanceFormsLabel.ForeColor = Color.FromArgb(37, 42, 69);
+            attendanceFormsLabel.Location = new Point(49, 49);
             attendanceFormsLabel.Name = "attendanceFormsLabel";
-            attendanceFormsLabel.Size = new Size(208, 32);
+            attendanceFormsLabel.Size = new Size(260, 41);
             attendanceFormsLabel.TabIndex = 0;
             attendanceFormsLabel.Text = "Attendance Forms";
             // 
             // AttendanceForms_Listings
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(166, 176, 230);
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(914, 600);
             Controls.Add(attendanceFormPagePanel);
             FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(3, 4, 3, 4);
             Name = "AttendanceForms_Listings";
             Text = "Uttendance";
             attendanceFormPagePanel.ResumeLayout(false);
@@ -88,5 +206,16 @@ namespace UttendanceDesktop
         private Panel attendanceFormPagePanel;
         private Label attendanceFormsLabel;
         private Label listingsLabel;
+        private Label statusLabel;
+        private Button editButton;
+        private Label dateLabel;
+        private Label filterLabel;
+        private ComboBox filterDropdown;
+        private ComboBox statusDropDown;
+        private DateTimePicker dateTimePicker;
+        private CoursepageContent.attendanceFormItem attendanceFormItem1;
+        private FlowLayoutPanel attendanceflowLayoutPanel;
+        private Button filterButton;
+        private Button button1;
     }
 }
--- UttendanceDesktop/CoursepageContent/AttendanceForms_Listings.cs
+++ UttendanceDesktop/CoursepageContent/AttendanceForms_Listings.cs
@@ -1,20 +1,239 @@
-﻿using System;
+﻿using UttendanceDesktop.CoursepageContent;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Diagnostics.Metrics;
 
 namespace UttendanceDesktop
 {
-    public partial class AttendanceForms_Listings: Form
+    // Aendri: 3/28/2025
+    public partial class AttendanceForms_Listings : Form
     {
+        private string connectionString = "datasource=localhost;port=3306;username=root;password=kachowmeow;database=uttendance";
+        private int courseNum = 123456; //temp value 
+        private attendanceFormItem[] attendanceListItems;
+
+        //Aendri: 3/28/2025
         public AttendanceForms_Listings()
         {
             InitializeComponent();
+            PopulateAttendanceFormList();
+        }
+
+        // Aendri: Populate the list of Attendence Form Items
+        private void PopulateAttendanceFormList()
+        {
+            // Open Connection to Database
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            MySqlCommand cmd;
+            MySqlDataReader reader;
+            int numAttendanceForms;
+
+            try
+            {
+                connection.Open();
+
+                // Get a count of all forms associated with this course
+                cmd = new MySqlCommand(
+                    "SELECT COUNT(*)" +
+                    "FROM form " +
+                    "WHERE FK_CourseNum=@CourseNum "
+                    , connection);
+                cmd.Parameters.AddWithValue("@CourseNum", courseNum);
+
+                // Process result
+                object result = cmd.ExecuteScalar();
+                if (result == null)
+                {
+                    System.Diagnostics.Debug.WriteLine("Null - NO FORMS FOUND!");
+                    return;
+                }
+                numAttendanceForms = Convert.ToInt32(result);
+
+                // Exit if NO Attendence forms
+                if (numAttendanceForms == 0) { Console.WriteLine("NO FORMS FOUND!"); return; }
+
+                // CREATE ATTENDENCE FORM OBJECTS:
+                attendanceListItems = new attendanceFormItem[numAttendanceForms];
+
+                // Clear the flow layout panel
+                if (attendanceflowLayoutPanel.Controls.Count > 0)
+                {
+                    attendanceflowLayoutPanel.Controls.Clear();
+                }
+
+                // Get a list of all forms associated with this course
+                cmd = new MySqlCommand(
+                        "SELECT FormID, ReleaseDateTime, CloseDateTime " +
+                        "FROM form " +
+                        "WHERE FK_CourseNum=@CourseNum "
+                    , connection);
+                cmd.Parameters.AddWithValue("@CourseNum", courseNum);
+                reader = cmd.ExecuteReader();
+
+                // Process results
+                int i = 0;
+                attendanceFormItem.AttendenceFormStatusOptions status;
+                DateTime start;
+                DateTime end;
+                DateTime currentTime = DateTime.Now;
+                while (reader.Read() && i < attendanceListItems.Length)
+                {
+
+                    attendanceListItems[i] = new attendanceFormItem();
+
+                    attendanceListItems[i].FormID = Convert.ToInt32(reader[0]);
+                    attendanceListItems[i].Date = Convert.ToDateTime(reader[1]); //DateOnly.Parse(reader.GetString(1));
+
+                    start = Convert.ToDateTime(reader[1]);
+
+                    // CALCULATE FORM STATUS:
+                    if (reader[2] == null) // Form has no end time
+                    {
+                        if (start > currentTime) // releases later 
+                        {
+                            status = attendanceFormItem.AttendenceFormStatusOptions.Upcoming;
+                        }
+                        else //released
+                        {
+                            status = attendanceFormItem.AttendenceFormStatusOptions.Open;
+                        }
+
+                    }
+                    else // Form has a start and end time
+                    {
+                        end = Convert.ToDateTime(reader[2]);
+
+                        // start, end, curr
+                        // 1. start end curr -> closed
+                        // 2. start curr end -> open
+                        // 3. end start curr -> invalid
+                        // 4. end curr start -> invalid
+                        // 5. curr end start -> invalid
+                        // 6. curr start end -> upcoming
+
+                        if (start > end)
+                        {
+                            System.Diagnostics.Debug.WriteLine("ERROR: in AttendanceForms_Listings.cs/PopulateAttendanceFormList(): Start date cannot be greater than end date! (FormID = " + attendanceListItems[i].FormID + ")");
+                            connection.Close();
+                            return;
+                        }
+                        else if (end < currentTime) // released and closed
+                        {
+                            status = attendanceFormItem.AttendenceFormStatusOptions.Closed;
+                        }
+                        else if (start > currentTime) // releases later
+                        {
+                            status = attendanceFormItem.AttendenceFormStatusOptions.Upcoming;
+                        }
+                        else // released and not closed
+                        {
+                            status = attendanceFormItem.AttendenceFormStatusOptions.Open;
+                        }
+                    }
+
+                    attendanceListItems[i].Status = status;
+
+                    // add object to panel
+                    attendanceflowLayoutPanel.Controls.Add(attendanceListItems[i]);
+
+                    i++;
+                }
+
+                reader.Close();
+
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("ERROR: AttendanceForms_Listings.cs/PopulateAttendanceFormList() : " + ex.ToString());
+                connection.Close();
+                return;
+            }
+
+            connection.Close();
+        }
+
+        //Aendri 4/3/2025
+        // On click, filter forms displayed based on the filter and date
+        // filterDropdown, dateTimePicker, statusDropDown
+        private void filterButton_Click(object sender, EventArgs e)
+        {
+            int status;
+            int filter;
+            //Set Status
+            if (statusDropDown.SelectedItem == null) status = -1;
+            else { status = statusDropDown.SelectedIndex; }
+            //Set Filter
+            if (filterDropdown.SelectedItem == null) filter = -1;
+            else { filter = filterDropdown.SelectedIndex; }
+ 
+            DateTime time = dateTimePicker.Value;
+
+            PopulateAttendanceFormList(); //REFRESH LIST
+
+            // Clear the flow layout panel
+            if (attendanceflowLayoutPanel.Controls.Count > 0)
+            {
+                attendanceflowLayoutPanel.Controls.Clear();
+            }
+
+            // Iterate through AttendenceListItems and ONLY add items that match the filter
+            for (int i = 0; i < attendanceListItems.Length; i++)
+            {
+                if (DoesMatchFilter(attendanceListItems[i], status, filter, time))
+                {
+                    attendanceflowLayoutPanel.Controls.Add(attendanceListItems[i]);
+                }
+            }
+        }
+
+        //Aendri 4/3/2025
+        // Returns a boolean indicating if the item matches the filter or not
+        private bool DoesMatchFilter(attendanceFormItem item, int status, int filter, DateTime time)
+        {
+            bool matchFilter;
+            bool matchStatus;
+
+            // Time Date filter
+            switch (filter)
+            {
+                case 0: //BEFORE
+                    matchFilter = item.Date.Date < time.Date;
+                    break;
+                case 1: //AFTER
+                    matchFilter = item.Date.Date > time.Date;
+                    break;
+                case 2: //ON
+                    matchFilter = item.Date.Date == time.Date;
+                    break;
+                default: //ALL
+                    matchFilter = true;
+                    break;
+            }
+
+            // Status Filter
+            switch (status)
+            {
+                case 0: //UPCOMING
+                    matchStatus = item.Status == attendanceFormItem.AttendenceFormStatusOptions.Upcoming;
+                    break;
+                case 1: //OPEN
+                    matchStatus = item.Status == attendanceFormItem.AttendenceFormStatusOptions.Open;
+                    break;
+                case 2: //CLOSED
+                    matchStatus = item.Status == attendanceFormItem.AttendenceFormStatusOptions.Closed;
+                    break;
+                default: //ALL
+                    matchStatus = true;
+                    break;
+            }
+
+            return matchStatus && matchFilter; //True if both cases are true
         }
     }
 }
