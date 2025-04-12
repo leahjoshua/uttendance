705338b019e996af5884330de4c5ff058d8813a8
JudYang4319 <judyang4319@gmail.com>
2025-04-11 20:10:26 -0500

    Dependency Injection

--- UttendanceWebsite/student.api/Program.cs
+++ UttendanceWebsite/student.api/Program.cs
@@ -1,6 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using student.api.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+/* Written by Judy Yang for CS 4485.0w1, CS Project, starting April 11, 2025
+    NetID: JXY200013
+*/
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    options.UseSqlServer(connectionString);
+
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
