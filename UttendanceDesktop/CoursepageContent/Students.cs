e605cc87f28c274fa11e7a842534c102e267ad6b
JoannaYang178 <joannayang178@gmail.com>
2025-04-13 22:32:37 -0500

    move student database queries to a student DAO

--- UttendanceDesktop/CoursepageContent/Students.cs
+++ UttendanceDesktop/CoursepageContent/Students.cs
@@ -17,7 +17,6 @@ namespace UttendanceDesktop
     // NetID: jxy210012
     public partial class Students : Form
     {
-        private static readonly string connectionString = GlobalResource.CONNECTION_STRING;
         private static readonly int courseNum = GlobalResource.CURRENT_CLASS_ID;
 
         //Sets up parameter for import module
@@ -46,49 +45,8 @@ namespace UttendanceDesktop
         //Pulls the list of all students enrolled in the current class and displays it on the data grid
         private void PopulateStudentTable()
         {
-            DataTable dataTable = new DataTable();
-
-            //Create the columns
-            for (int i = 0; i < displayList.Length; i++)
-            {
-                dataTable.Columns.Add(displayList[i]);
-            }
-
-            try
-            {
-                //Open database connection
-                MySqlConnection connection = new MySqlConnection(connectionString);
-                connection.Open();
-                //Select all student information from all students enrolled in the current class
-                var query = "SELECT student.* FROM student " +
-                    "INNER JOIN attends ON student.UTDID=attends.FK_UTDID " +
-                    "WHERE attends.FK_CourseNum=" + GlobalResource.CURRENT_CLASS_ID +
-                    " ORDER BY student.SLName;";
-
-                //Execute query
-                MySqlCommand cmd = new MySqlCommand(query, connection);
-                //Read result
-                MySqlDataReader databaseReader = cmd.ExecuteReader();
-                while (databaseReader.Read())
-                {
-                    var row = dataTable.NewRow();
-
-                    row[displayList[0]] = databaseReader["SLName"].ToString();
-                    row[displayList[1]] = databaseReader["SFName"].ToString();
-                    row[displayList[2]] = databaseReader["SNetID"].ToString();
-                    row[displayList[3]] = databaseReader["UTDID"].ToString();
-
-                    dataTable.Rows.Add(row);
-                }
-                databaseReader.Close();
-                connection.Close();
-                //Send data to data table
-                this.studentTable.DataSource = dataTable;
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Failed to populate.");
-            }
+            StudentsDAO studentInfo = new StudentsDAO();
+            this.studentTable.DataSource = studentInfo.getAllStudentInfo(displayList, courseNum);
         }
 
         //Displays the options for adding students
@@ -139,50 +97,18 @@ namespace UttendanceDesktop
                 DialogResult result = MessageBox.Show(confirmMsg, "Confirmation", MessageBoxButtons.OKCancel);
                 if (result == DialogResult.OK)
                 {
-                    //Open database connection
-                    MySqlConnection connection = new MySqlConnection(connectionString);
-                    connection.Open();
-
+                    StudentsDAO studentInfo = new StudentsDAO();
                     for (int i = 0; i < numRows; i++)
                     {
                         DataGridViewRow selectedRow = studentTable.SelectedRows[i];
                         //Get the primary key of the selected row
                         string utdID = selectedRow.Cells["UTD-ID"].Value.ToString();
-                        removeStudent(utdID, connection);
+                        studentInfo.removeStudentsFromClass(utdID, courseNum);
                     }
-
-                    connection.Close();
                     PopulateStudentTable();
                 }
             }
 
-        }
-
-        //Remove a student from the class given their UTD-ID
-        private void removeStudent(string id, MySqlConnection connection)
-        {
-            //Remove student from the attends table
-            MySqlCommand cmd = new MySqlCommand("DELETE FROM attends WHERE FK_UTDID=@fkUtdID AND FK_CourseNum=@courseNum;", connection);
-            cmd.Parameters.AddWithValue("@fkUtdID", id);
-            cmd.Parameters.AddWithValue("@courseNum", courseNum);
-            cmd.ExecuteNonQuery();
-
-            //Check if the student is enrolled in any other classes
-            cmd = new MySqlCommand("SELECT COUNT(*) FROM attends WHERE FK_UTDID=@fkUtdID2;", connection);
-            cmd.Parameters.AddWithValue("@fkUtdID2", id);
-            //Read result
-            MySqlDataReader reader = cmd.ExecuteReader();
-            reader.Read();
-            int count = reader.GetInt32(0);
-            reader.Close();
-
-            //If not, then remove them from the student table
-            if(count == 0)
-            {
-                cmd = new MySqlCommand("DELETE FROM student WHERE UTDID=@utdID;", connection);
-                cmd.Parameters.AddWithValue("@utdID", id);
-                cmd.ExecuteNonQuery();
-            }
         }
     }
 }
---
+++ UttendanceDesktop/CoursepageContent/StudentsDAO.cs
@@ -0,0 +1,106 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static UttendanceDesktop.GlobalResource;
+
+namespace UttendanceDesktop.CoursepageContent
+{
+    //started 4/13/2025
+    internal class StudentsDAO
+    {
+        private static readonly string connectionString = GlobalResource.CONNECTION_STRING;
+
+        //Remove a student from a specific class given their UTD-ID
+        public void removeStudentsFromClass(string id, int courseNum)
+        {
+            //Open database connection
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            connection.Open();
+
+            //Remove student from the attends table
+            MySqlCommand cmd = new MySqlCommand("DELETE FROM attends WHERE FK_UTDID=@fkUtdID AND FK_CourseNum=@courseNum;", connection);
+            cmd.Parameters.AddWithValue("@fkUtdID", id);
+            cmd.Parameters.AddWithValue("@courseNum", courseNum);
+            cmd.ExecuteNonQuery();
+
+            //Check if the student is enrolled in any other classes
+            cmd = new MySqlCommand("SELECT COUNT(*) FROM attends WHERE FK_UTDID=@fkUtdID2;", connection);
+            cmd.Parameters.AddWithValue("@fkUtdID2", id);
+            //Read result
+            MySqlDataReader reader = cmd.ExecuteReader();
+            reader.Read();
+            int count = reader.GetInt32(0);
+            reader.Close();
+            connection.Close();
+
+            //If not, then remove them from the student table
+            if (count == 0)
+            {
+                removeStudentFromDB(id);
+            }
+
+        }
+
+        //Removes a student from the student table in the database
+        private void removeStudentFromDB(string id)
+        {
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            connection.Open();
+
+            MySqlCommand cmd = new MySqlCommand("DELETE FROM student WHERE UTDID=@utdID;", connection);
+            cmd.Parameters.AddWithValue("@utdID", id);
+            cmd.ExecuteNonQuery();
+
+            connection.Close();
+        }
+
+        public DataTable getAllStudentInfo(string[] displayList, int courseNum)
+        {
+            DataTable dataTable = new DataTable();
+
+            //Create the columns
+            for (int i = 0; i < displayList.Length; i++)
+            {
+                dataTable.Columns.Add(displayList[i]);
+            }
+
+            //Open database connection
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            connection.Open();
+            //Select all student information from all students enrolled in the current class
+            var query = "SELECT student.* FROM student " +
+                "INNER JOIN attends ON student.UTDID=attends.FK_UTDID " +
+                "WHERE attends.FK_CourseNum=" + GlobalResource.CURRENT_CLASS_ID +
+                " ORDER BY student.SLName;";
+
+            //Execute query
+            MySqlCommand cmd = new MySqlCommand(query, connection);
+            //Read result
+            using (MySqlDataReader databaseReader = cmd.ExecuteReader())
+            {
+                while (databaseReader.Read())
+                {
+                    var row = dataTable.NewRow();
+
+                    row[displayList[0]] = databaseReader["SLName"].ToString();
+                    row[displayList[1]] = databaseReader["SFName"].ToString();
+                    row[displayList[2]] = databaseReader["SNetID"].ToString();
+                    row[displayList[3]] = databaseReader["UTDID"].ToString();
+
+                    dataTable.Rows.Add(row);
+                }
+
+            }
+
+            connection.Close();
+            //Send data to data table
+            return dataTable;
+        }
+
+    }
+}
