f1ba903e39293c27e968f18ffc5583fa041e2deb
Leah Joshua <leahjoshua1313@gmail.com>
2025-04-17 18:06:01 -0500

    design tweaks to import question modal

--- UttendanceDesktop/CoursepageContent/CreateAttendanceForm/ImportQuestionModal.Designer.cs
+++ UttendanceDesktop/CoursepageContent/CreateAttendanceForm/ImportQuestionModal.Designer.cs
@@ -43,26 +43,29 @@
             importPanel.Controls.Add(qBankDropdown);
             importPanel.Controls.Add(qBankLabel);
             importPanel.Location = new Point(-3, 0);
+            importPanel.Margin = new Padding(3, 2, 3, 2);
             importPanel.Name = "importPanel";
-            importPanel.Size = new Size(592, 488);
+            importPanel.Size = new Size(584, 421);
             importPanel.TabIndex = 0;
             // 
             // importFlowPanel
             // 
-            importFlowPanel.Location = new Point(30, 116);
-            importFlowPanel.MaximumSize = new Size(538, 324);
-            importFlowPanel.MinimumSize = new Size(538, 324);
+            importFlowPanel.Location = new Point(44, 110);
+            importFlowPanel.Margin = new Padding(3, 2, 3, 2);
+            importFlowPanel.MaximumSize = new Size(500, 500);
+            importFlowPanel.MinimumSize = new Size(20, 20);
             importFlowPanel.Name = "importFlowPanel";
-            importFlowPanel.Size = new Size(538, 324);
+            importFlowPanel.Size = new Size(500, 254);
             importFlowPanel.TabIndex = 13;
             // 
             // qBankDropdown
             // 
             qBankDropdown.Font = new Font("Segoe UI", 12F);
             qBankDropdown.FormattingEnabled = true;
-            qBankDropdown.Location = new Point(30, 64);
+            qBankDropdown.Location = new Point(44, 65);
+            qBankDropdown.Margin = new Padding(3, 2, 3, 2);
             qBankDropdown.Name = "qBankDropdown";
-            qBankDropdown.Size = new Size(538, 36);
+            qBankDropdown.Size = new Size(500, 29);
             qBankDropdown.TabIndex = 12;
             qBankDropdown.SelectedIndexChanged += qBankDropdown_SelectedIndexChanged;
             // 
@@ -70,20 +73,21 @@
             // 
             qBankLabel.AutoSize = true;
             qBankLabel.BackColor = Color.FromArgb(166, 176, 230);
-            qBankLabel.Font = new Font("Segoe UI", 13F);
+            qBankLabel.Font = new Font("Segoe UI", 15F);
             qBankLabel.ForeColor = Color.FromArgb(37, 42, 69);
-            qBankLabel.Location = new Point(24, 21);
+            qBankLabel.Location = new Point(44, 25);
             qBankLabel.Name = "qBankLabel";
-            qBankLabel.Size = new Size(217, 30);
+            qBankLabel.Size = new Size(195, 28);
             qBankLabel.TabIndex = 11;
             qBankLabel.Text = "Question Bank Name";
             // 
             // ImportQuestionModal
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(589, 487);
+            ClientSize = new Size(581, 420);
             Controls.Add(importPanel);
+            Margin = new Padding(3, 2, 3, 2);
             MaximizeBox = false;
             MinimizeBox = false;
             Name = "ImportQuestionModal";
