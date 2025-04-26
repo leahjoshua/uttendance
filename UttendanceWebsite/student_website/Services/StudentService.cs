34c1a61c654614602599da800bb86e2b7bb4a905
JudYang4319 <judyang4319@gmail.com>
2025-04-25 22:35:57 -0500

    Updatedd StudentService comment

--- UttendanceWebsite/student_website/Services/StudentService.cs
+++ UttendanceWebsite/student_website/Services/StudentService.cs
@@ -5,7 +5,7 @@
         /* Written by Judy Yang for CS 4485.0w1, CS Project, starting April 25, 2025
          * NetID: JXY200013
          * 
-         * Service for Student to handle databse interactions
+         * Service for allowing Student variables to be accessible globally
          */
         public int StudentID { get; set; }
     }
