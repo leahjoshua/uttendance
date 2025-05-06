fd9c8984c771a962dd07dcc97a17d32499b549ec
rixarixaaa <111485215+rixarixaaa@users.noreply.github.com>
2025-05-05 22:52:01 -0500

    Update Answers.cs

--- UttendanceWebsite/student_website/Models/Answers.cs
+++ UttendanceWebsite/student_website/Models/Answers.cs
@@ -3,9 +3,8 @@ using System.ComponentModel.DataAnnotations;
 
 namespace student_website.Models
 {
-    /* Written by Parisa Nawar for CS 4485.0w1, CS Project, starting May 3, 2025
-     * NetID: PXN210032
-     * 
+    /* Written by Judy Yang and Parisa Nawar for CS 4485.0w1, CS Project, starting May 3, 2025
+     * NetID: JXY200013, PXN210032
      * Class entity corresponding to the Answers table in the database.
      */
     public class Answers
