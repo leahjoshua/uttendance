6588b673cc321c40c319540681fb12f68bd73dea
rixarixaaa <111485215+rixarixaaa@users.noreply.github.com>
2025-05-08 21:12:17 -0500

    Update README.md

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # UTtenDance
-
 A UTD Attendance Application.
 
 ### Team Members
@@ -9,7 +8,35 @@ A UTD Attendance Application.
 - Joanna Yang
 - Judy Yang
 
-### Documentation Guides (In Progress)
-| Guide | Description |
-| ------------- | ------------- |
-| [Database Connection Guide](./Readable%20Files/DBCnctGuide.md) | Guide to install database software, connect database to application. |
+## Database Connection Guide
+1. Download [MariaDB](https://mariadb.org/download/).
+2. Open up the MariaDB Installer, and click Next on every page, except for Default Instance Properties.
+	- Modify root password to be kachowmeow.
+3. Once done, keep clicking Next until MariaDB has been installed. 
+4. Install [HeidiSQL](https://www.heidisql.com/download.php).
+	- Keep clicking Next until the application has been installed. Modify nothing. 
+5. Launch HeidiSQL.
+6. Open up a new session.
+	- User should be root.
+	- Password should be kachowmeow.
+	- Port should be 3306.
+7. Click Open to start the session.
+8. On the top left corner, hover to File > click on Load SQL File.
+9. Navigate through the File System to load up create_uttendance_DB.sql.
+10. Select the blue triangle on the top of HeidiSQL banner to execute the SQL Script.
+
+## Desktop Application Guide
+1. Once the SQL Script is successfully running, open up Visual Studio 2022 (make sure this is the latest version).
+	- Otherwise, [install](https://visualstudio.microsoft.com/downloads/) it.
+2. In File Explorer, navigate to UttendanceDesktop.sln.
+	- Open this file using Visual Studio 2022.
+3. Once everything is loaded up from this file in Visual Studio, select Start Without Debugging option in the top banner.
+4. The desktop application should open up.
+    - You can either create a new account, or use username 'sxh39228' and password 'blowupmind' for quick demo.
+
+## Website Guide
+1. In File Explorer, navigate to student_website.sln.
+	- Open this file using Visual Studio 2022.
+2. Once everything is loaded up from this file in Visual Studio, select Start option in the top banner.
+3. In your default browser, the website should open up.
+    - Use UTDID '2021345555' and password 'corn' for quick demo. 
--- create_uttendance_DB.sql
+++ create_uttendance_DB.sql
@@ -32,35 +32,53 @@ CREATE TABLE IF NOT EXISTS `answerchoice` (
 
 -- Dumping data for table uttendance.answerchoice: ~29 rows (approximately)
 INSERT INTO `answerchoice` (`AnswerID`, `AnswerStatement`, `IsCorrect`, `FK_QuestionID`) VALUES
-	(1, 'sdf', b'1', 1),
-	(2, 'sdf', b'0', 1),
-	(3, 'test', b'1', 5),
-	(4, 'test2', b'0', 5),
-	(5, 'A', b'0', 6),
-	(6, 'B', b'0', 6),
-	(7, 'C', b'1', 6),
-	(8, 'D', b'0', 6),
-	(9, '-1', b'0', 8),
-	(10, '2', b'0', 8),
-	(11, '25', b'0', 8),
-	(12, '999999999999999999999999999', b'1', 8),
-	(13, 'fdssdf', b'1', 9),
-	(14, 'sdfsdf', b'0', 9),
-	(15, 'the ORV carrd', b'1', 2),
-	(16, 'youtube', b'0', 2),
-	(17, 'pacific ocean', b'0', 2),
-	(18, '3', b'1', 3),
-	(19, 'nothing bro :(', b'1', 4),
-	(20, 'dfgdfg', b'0', 10),
-	(21, 'dfgdfg', b'1', 10),
-	(22, 'sdf', b'1', 11),
-	(23, 'sdfsdf', b'0', 11),
-	(24, 'dfsdfdsf', b'1', 14),
-	(25, 'sdfsdf', b'0', 14),
-	(26, 'Blue', b'0', 15),
-	(27, 'Green', b'0', 15),
-	(28, 'Yellow', b'0', 15),
-	(29, 'She doesn\'t know', b'1', 15);
+	(1, 'Mitochondria', b'1', 1),
+	(2, 'Ribbed Cells', b'0', 1),
+	(3, 'Cell', b'1', 5),
+	(4, 'DNA', b'0', 5),
+	(5, 'Cell Membrane', b'0', 6),
+	(6, 'Cytoplasm', b'0', 6),
+	(7, 'Nucleus', b'1', 6),
+	(8, 'Cell Cycle', b'0', 6),
+	(9, 'Mitochondrion', b'0', 8),
+	(10, 'Ecosystem', b'0', 8),
+	(11, 'Adaptation', b'0', 8),
+	(12, 'Species', b'1', 8),
+	(13, 'Population', b'1', 9),
+	(14, 'Taxonomy', b'0', 9),
+	(15, 'Phylogeny', b'1', 2),
+	(16, 'Biochemistry', b'0', 2),
+	(17, 'Genetics', b'0', 2),
+	(18, 'Decomposer', b'1', 3),
+	(19, 'Drug Base', b'1', 4),
+	(20, 'Ectoderm', b'0', 10),
+	(21, 'Electrochemical Gradient', b'1', 10),
+	(22, 'Electron Acceptor', b'1', 3),
+	(23, 'Electron Carrier', b'0', 3),
+	(24, 'Electron Donor', b'1', 3),
+	(25, 'Jejunum', b'0', 14),
+	(26, 'Lipid', b'0', 15),
+	(27, 'Pheromone', b'0', 15),
+	(28, 'Protist', b'0', 15),
+	(29, 'Ribosome', b'1', 15);
+
+-- Dumping structure for table uttendance.answers
+CREATE TABLE IF NOT EXISTS `answers` (
+  `FK_AnswerID` int(11) NOT NULL,
+  `FK_SubmissionID` int(11) NOT NULL,
+  PRIMARY KEY (`FK_AnswerID`,`FK_SubmissionID`),
+  KEY `FK_AnswerID` (`FK_AnswerID`),
+  KEY `answers_ibfk_2` (`FK_SubmissionID`),
+  CONSTRAINT `answers_ibfk_1` FOREIGN KEY (`FK_AnswerID`) REFERENCES `answerchoice` (`AnswerID`),
+  CONSTRAINT `answers_ibfk_2` FOREIGN KEY (`FK_SubmissionID`) REFERENCES `submission` (`submissionID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+-- Dumping data for table uttendance.answers: ~4 rows (approximately)
+INSERT INTO `answers` (`FK_AnswerID`, `FK_SubmissionID`) VALUES
+	(18, 16),
+	(22, 17),
+	(27, 16),
+	(28, 17);
 
 -- Dumping structure for table uttendance.attends
 CREATE TABLE IF NOT EXISTS `attends` (
@@ -72,9 +90,8 @@ CREATE TABLE IF NOT EXISTS `attends` (
   CONSTRAINT `attends_ibfk_2` FOREIGN KEY (`FK_CourseNum`) REFERENCES `class` (`CourseNum`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 
--- Dumping data for table uttendance.attends: ~9 rows (approximately)
+-- Dumping data for table uttendance.attends: ~8 rows (approximately)
 INSERT INTO `attends` (`FK_UTDID`, `FK_CourseNum`) VALUES
-	(4343432, 123456),
 	(2021070921, 123456),
 	(2021188666, 123456),
 	(2021308444, 123456),
@@ -91,19 +108,16 @@ CREATE TABLE IF NOT EXISTS `class` (
   `ClassSubject` varchar(5) DEFAULT NULL,
   `ClassNum` int(11) DEFAULT NULL,
   `ClassName` varchar(40) DEFAULT NULL,
-  `FK_ImageID` int(11) DEFAULT NULL,
   `ClassStartTime` time DEFAULT NULL,
   `ClassEndTime` time DEFAULT NULL,
-  PRIMARY KEY (`CourseNum`),
-  KEY `FK_ImageID` (`FK_ImageID`),
-  CONSTRAINT `class_ibfk_1` FOREIGN KEY (`FK_ImageID`) REFERENCES `images` (`ImageID`)
+  PRIMARY KEY (`CourseNum`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 
 -- Dumping data for table uttendance.class: ~3 rows (approximately)
-INSERT INTO `class` (`CourseNum`, `SectionNum`, `ClassSubject`, `ClassNum`, `ClassName`, `FK_ImageID`, `ClassStartTime`, `ClassEndTime`) VALUES
-	(12345, 1, 'CS', 4349, 'Advanced Algorithms', NULL, '14:30:00', '15:45:00'),
-	(123456, 1, 'CS', 4384, 'Operating Systems', NULL, '10:00:00', '11:15:00'),
-	(999999, 1, 'CS', 9999, 'Test Class', NULL, '11:30:00', '12:45:00');
+INSERT INTO `class` (`CourseNum`, `SectionNum`, `ClassSubject`, `ClassNum`, `ClassName`, `ClassStartTime`, `ClassEndTime`) VALUES
+	(12345, 1, 'CS', 4349, 'Advanced Algorithms', '14:30:00', '15:45:00'),
+	(123456, 1, 'CS', 4384, 'Operating Systems', '10:00:00', '11:15:00'),
+	(999999, 1, 'CS', 9999, 'Computer Networking', '11:30:00', '12:45:00');
 
 -- Dumping structure for table uttendance.form
 CREATE TABLE IF NOT EXISTS `form` (
@@ -119,11 +133,11 @@ CREATE TABLE IF NOT EXISTS `form` (
 
 -- Dumping data for table uttendance.form: ~5 rows (approximately)
 INSERT INTO `form` (`FormID`, `PassWd`, `ReleaseDateTime`, `CloseDateTime`, `FK_CourseNum`) VALUES
-	(1, 'dfdsf', '2025-04-12 13:44:47', '2025-04-19 13:44:47', 123456),
-	(2, 'test', '2025-04-12 13:47:56', '2025-04-16 13:47:56', 123456),
-	(3, 'PLEASE WORK', '2025-04-19 10:41:36', '2025-04-26 10:41:36', 123456),
-	(4, 'jotest', '2025-04-15 13:40:13', '2025-04-17 13:40:21', 123456),
-	(5, 'letest', '2025-04-15 13:40:17', '2025-04-17 13:40:21', 123456);
+	(1, 'potato', '2025-04-12 10:00:00', '2025-05-22 11:30:00', 123456),
+	(2, 'tomato', '2025-04-14 10:00:00', '2025-06-14 11:30:00', 123456),
+	(3, 'corn', '2025-04-16 10:00:00', '2025-05-16 11:30:00', 123456),
+	(4, 'cake', '2025-05-01 10:00:00', '2025-03-18 11:30:00', 123456),
+	(5, 'broccoli', '2025-05-21 10:00:00', '2025-04-21 11:30:00', 123456);
 
 -- Dumping structure for table uttendance.has
 CREATE TABLE IF NOT EXISTS `has` (
@@ -140,15 +154,6 @@ INSERT INTO `has` (`FK_FormID`, `FK_QuestionID`) VALUES
 	(3, 3),
 	(3, 15);
 
--- Dumping structure for table uttendance.images
-CREATE TABLE IF NOT EXISTS `images` (
-  `ImageID` int(11) NOT NULL,
-  `ImageBits` varbinary(256) DEFAULT NULL,
-  PRIMARY KEY (`ImageID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
-
--- Dumping data for table uttendance.images: ~0 rows (approximately)
-
 -- Dumping structure for table uttendance.instructor
 CREATE TABLE IF NOT EXISTS `instructor` (
   `INetID` varchar(9) NOT NULL,
@@ -160,8 +165,8 @@ CREATE TABLE IF NOT EXISTS `instructor` (
 
 -- Dumping data for table uttendance.instructor: ~2 rows (approximately)
 INSERT INTO `instructor` (`INetID`, `IFName`, `ILName`, `IPassword`) VALUES
-	('mxm123456', 'Meow', 'Meowington', 'password'),
-	('SXH210003', 'Sooyoung', 'Han', 'sunfish');
+	('mxm123456', 'Milly', 'Meron', 'h4Ga!'),
+	('sxh39228', 'Sally', 'Hemmings', 'blowupmind');
 
 -- Dumping structure for table uttendance.qbank
 CREATE TABLE IF NOT EXISTS `qbank` (
@@ -175,11 +180,11 @@ CREATE TABLE IF NOT EXISTS `qbank` (
 
 -- Dumping data for table uttendance.qbank: ~5 rows (approximately)
 INSERT INTO `qbank` (`BankID`, `BankTitle`, `FK_INetID`) VALUES
-	(1, 'Omnisicient Reader', 'SXH210003'),
-	(2, 'Three Ways to Survive in a Ruined World', 'SXH210003'),
-	(3, 'I\'m Kim Dokja', 'SXH210003'),
-	(4, 'Hehehe', 'mxm123456'),
-	(5, 'Kekeke', 'mxm123456');
+	(1, 'Biology', 'sxh39228'),
+	(2, 'Chemistry', 'sxh39228'),
+	(3, 'Cellular', 'sxh39228'),
+	(4, 'Molecules', 'mxm123456'),
+	(5, 'Physics', 'mxm123456');
 
 -- Dumping structure for table uttendance.question
 CREATE TABLE IF NOT EXISTS `question` (
@@ -191,23 +196,13 @@ CREATE TABLE IF NOT EXISTS `question` (
   CONSTRAINT `FK_question_qbank` FOREIGN KEY (`FK_BankID`) REFERENCES `qbank` (`BankID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 
--- Dumping data for table uttendance.question: ~15 rows (approximately)
+-- Dumping data for table uttendance.question: ~5 rows (approximately)
 INSERT INTO `question` (`QuestionID`, `ProblemStatement`, `FK_BankID`) VALUES
-	(1, 'sdf', NULL),
-	(2, 'Where can you find the whole ORV webnovel?', 1),
-	(3, 'What\'s Kim Dokja\'s Favorite turn?', 3),
-	(4, 'What is wrong with you bro?', 1),
-	(5, 'dfgdfg', NULL),
-	(6, 'test', NULL),
-	(7, 'do u like joannas plant', NULL),
-	(8, 'what is aendris iq', NULL),
-	(9, 'testq', NULL),
-	(10, 'indiviudla question', NULL),
-	(11, 'sdfsf', NULL),
-	(12, 'NEW QUESTION', NULL),
-	(13, 'sdf', NULL),
-	(14, 'sdf', NULL),
-	(15, 'what\'s joanna\'s favorite color', NULL);
+	(1, 'What is the hypothesis biology?', NULL),
+	(2, 'Where is the stem cell located?', 1),
+	(3, 'What is the largest nucleus structure?', 3),
+	(4, 'How does molecule change between states?', 1),
+	(15, 'How does an acid produce liposome?', NULL);
 
 -- Dumping structure for table uttendance.student
 CREATE TABLE IF NOT EXISTS `student` (
@@ -218,10 +213,9 @@ CREATE TABLE IF NOT EXISTS `student` (
   PRIMARY KEY (`UTDID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 
--- Dumping data for table uttendance.student: ~9 rows (approximately)
+-- Dumping data for table uttendance.student: ~8 rows (approximately)
 INSERT INTO `student` (`UTDID`, `SNetID`, `SFName`, `SLName`) VALUES
-	(4343432, 'aes11111', 'aendri', 'singh'),
-	(2021070921, 'jxy123456', 'Y', 'Joobie'),
+	(2021070921, 'jxy123456', 'Judy', 'Yan'),
 	(2021188666, 'zjd130000', 'Zach', 'Dewey'),
 	(2021308444, 'cab160444', 'Chase', 'Burrell'),
 	(2021345555, 'nkc160199', 'Kevin', 'Chen'),
@@ -243,25 +237,12 @@ CREATE TABLE IF NOT EXISTS `submission` (
   KEY `FK_UTDID` (`FK_UTDID`),
   CONSTRAINT `submission_ibfk_1` FOREIGN KEY (`FK_FormID`) REFERENCES `form` (`FormID`),
   CONSTRAINT `submission_ibfk_2` FOREIGN KEY (`FK_UTDID`) REFERENCES `student` (`UTDID`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 
--- Dumping data for table uttendance.submission: ~15 rows (approximately)
+-- Dumping data for table uttendance.submission: ~2 rows (approximately)
 INSERT INTO `submission` (`SubmissionID`, `AttendanceStatus`, `IPAddress`, `DateTime`, `FK_FormID`, `FK_UTDID`) VALUES
-	(1, 'P', 'utd', NULL, 2, 4343432),
-	(2, 'E', NULL, NULL, 2, 2021070921),
-	(3, 'P', 'utd', NULL, 4, 2021188666),
-	(4, 'P', 'utd', NULL, 5, 2021308444),
-	(5, 'P', 'utd', NULL, 2, 2021188666),
-	(6, 'P', 'utd', NULL, 2, 2021308444),
-	(7, 'P', 'utd', NULL, 1, 2021504000),
-	(8, 'P', 'utd', NULL, 1, 2021393333),
-	(9, 'P', 'utd', NULL, 1, 2021542222),
-	(10, 'P', 'utd', NULL, 1, 2021308444),
-	(11, 'P', 'utd', NULL, 1, 2021345555),
-	(12, 'P', 'utd', NULL, 1, 2021188666),
-	(13, 'P', 'utd', NULL, 1, 2021070921),
-	(14, 'P', 'utd', NULL, 1, 2021482111),
-	(15, 'P', 'utd', NULL, 1, 4343432);
+	(16, 'P', '129.110.242.224', '2025-05-08 17:13:28', 3, 2021070921),
+	(17, 'P', '129.110.242.224', '2025-05-08 17:14:41', 3, 2021188666);
 
 -- Dumping structure for table uttendance.teaches
 CREATE TABLE IF NOT EXISTS `teaches` (
@@ -276,7 +257,6 @@ CREATE TABLE IF NOT EXISTS `teaches` (
 -- Dumping data for table uttendance.teaches: ~3 rows (approximately)
 INSERT INTO `teaches` (`FK_INetID`, `FK_CourseNum`) VALUES
 	('mxm123456', 12345),
-	('mxm123456', 123456),
 	('SXH210003', 123456),
 	('SXH210003', 999999);
 
